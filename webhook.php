c611155fd9c6667c1f1d0a0ef485ca6819361b74
rkvishwa <kk0116716@gmail.com>
2022-11-22 21:59:53 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -6,7 +6,14 @@ $getcode = "https://api.telegram.org/bot5817268721:AAG6qqTHLsAQtcjfeQm7n5B1TE4Wv
 $input = file_get_contents('php://input');
 $date = json_decode($input);
 $chat_id = $data->message->chat->id;
-$text = $data->message->text;
-echo $text;
+$text = $data->message->text." webhook reply";
+$token = '5817268721:AAG6qqTHLsAQtcjfeQm7n5B1TE4WvAk2i0o';
+$url = "https://api.telegram.org/bot$token/sendMessage?text=$text&chat_id=$chat_id";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+$result = curl_exec($ch);
+curl_close($ch);
 
  ?>
