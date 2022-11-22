fc554e4bf37e9aee54dbe41497f5148031e16de5
rkvishwa <kk0116716@gmail.com>
2022-11-22 23:12:03 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,24 +1,23 @@
 <?php 
 include('autoload.php');
 
-//$input = file_get_contents('php://input');
-//$data = json_decode($input);
-//$chat_id = $data->message->chat->id;
-//$text = $data->message->text;
-$text= 'hello';
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+$chat_id = $data->message->chat->id;
+$text = $data->message->text;
+
 if($text == '/start'){
 	$message = 'Welcome%0Adude';
 }elseif($text == '/code'){
 	$message = 'Enter <b>Password</b>';
 }else{
-	if(!is_user_exist($con,$message)){
-		$message = 'in valid pasword';
+	if(!is_user_exist($con,$text)){
+		$message = 'in valid password';
 	}else{
-		$message = "Hello ". is_user_exist($con,$message);
+		$message = "Hello ". is_user_exist($con,$text);
 	}
 }
 
-//send_message($chat_id,$message);
-echo $message;
+send_message($chat_id,$message);
 
  ?>
