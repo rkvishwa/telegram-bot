fa1e1f91020237b28f88236dfdb3071c0e4e0055
rkvishwa <kk0116716@gmail.com>
2022-11-22 22:05:54 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -12,7 +12,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $text;
+mail("vishwa@ayzack.com","webhook",$text);
 //$setcode = "https://api.telegram.org/bot5817268721:AAG6qqTHLsAQtcjfeQm7n5B1TE4WvAk2i0o/setWebhook?url=https://rvkbot.tk/webhook.php";
 //$getcode = "https://api.telegram.org/bot5817268721:AAG6qqTHLsAQtcjfeQm7n5B1TE4WvAk2i0o/getwebhookinfo";
 
