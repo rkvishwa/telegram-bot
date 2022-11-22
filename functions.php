ff45102c44a7dedac4dba6a152afccae72cf90df
rkvishwa <kk0116716@gmail.com>
2022-11-22 22:46:48 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 define('TOKEN','5817268721:AAG6qqTHLsAQtcjfeQm7n5B1TE4WvAk2i0o');
 
 function send_message($chat_id,$msg){
-	$url = "https://api.telegram.org/bot".TOKEN."/sendMessage?text=$msg&chat_id=$chat_id";
+	$url = "https://api.telegram.org/bot".TOKEN."/sendMessage?text=$msg&chat_id=$chat_id&parse_mode=html";
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
