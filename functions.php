6f244d15f4340a080f8effd07c4255feb559ec9c
rkvishwa <kk0116716@gmail.com>
2022-11-22 23:35:03 +0530

    update

--- functions.php
+++ functions.php
@@ -11,18 +11,15 @@ function send_message($chat_id,$msg){
 }
 
 function is_user_exist($con,$password){
-	$stmt = mysqli_stmt_init($con);
-	$sql = "SELECT user_name FROM user_info WHERE u_pw=?";
-	if(mysqli_stmt_prepare($stmt,$sql)){
-		mysqli_stmt_bind_param($stmt,'s',$password);
-		mysqli_stmt_execute($stmt);
-		$result = mysqli_stmt_get_result($stmt);
-		if(mysqli_num_rows($result) > 0){
-			$row = mysqli_fetch_assoc($result);
-			return $row['user_name'];
-		}
+	$text = mysqli_real_escape_string($con,$password);
+	$query = mysqli_query($con,"SELECT * FROM user_info WHERE u_pw='$text'");
+	if(mysqli_num_rows($query) > 0){
+		$row = mysqli_fetch_assoc($query);
+		$message = $row['user_name'];
+	}else{
+		$message = false;
 	}
-	return false;
+	return $message;
 }
 
  ?>
